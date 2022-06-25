a6bc822044ab79494a7f098e536b5f7fae3c6d75
Emira Shano <emira@eshano.com>
2022-06-25 18:14:08 -0400

    added AbilitiesView custom view

---
+++ Pocket-Dex/AbilitiesComponent/AbilitiesView.swift
@@ -0,0 +1,20 @@
+//
+//  AbilitiesView.swift
+//  Pocket-Dex
+//
+//  Created by Emira Hajj on 6/23/22.
+//
+
+import UIKit
+
+class AbilitiesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
